6292ac2f3a08033cb4ef90e277b85a82c63edbe1
Rahul Ramesh <rahulram@seas.upenn.edu>
2024-08-18 15:27:44 -0400

    add time and MFU to train logs

--- utils/nanogpt.py
+++ utils/nanogpt.py
@@ -131,6 +131,24 @@ class nanoGPT(nn.Module):
                 torch.nn.init.normal_(
                     p, mean=0.0, std=0.02/math.sqrt(2 * cfg.n_layer))
 
+    def estimate_mfu(self, fwdbwd_per_iter, dt):
+        """ estimate model flops utilization (MFU) in units of A100 bfloat16 peak FLOPS """
+        # first estimate the number of flops we do per iteration.
+        # see PaLM paper Appendix B as ref: https://arxiv.org/abs/2204.02311
+        flops_promised = {'a100': 312e12, 'a10': 125e12, '4090': 82.5} 
+
+        N = self.get_num_params()
+        cfg = self.config
+        L, H, Q, T = cfg.n_layer, cfg.n_head, cfg.n_embd//cfg.n_head, cfg.block_size
+        flops_per_token = 6*N + 12*L*H*Q*T
+        flops_per_fwdbwd = flops_per_token * T
+        flops_per_iter = flops_per_fwdbwd * fwdbwd_per_iter
+        # express our flops throughput as ratio of A100 bfloat16 peak flops
+        flops_achieved = flops_per_iter * (1.0/dt) # per second
+        flops_promised = 312e12 # A100 GPU bfloat16 peak flops is 312 TFLOPS
+        mfu = flops_achieved / flops_promised['a10']
+        return mfu
+
     def get_num_params(self, non_embedding=True):
         """
         Return the number of parameters in the model.
